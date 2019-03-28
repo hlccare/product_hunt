18d89120475a86703c7b771a7121bfd874ed34ea
hlccare <280108904@qq.com>
2019-03-28 14:11:21 +0800

    在product这个app中增加Product模型，在admin.py中注册model，进行migration；在setting中进行MEDIA路径设置，并添加至urls中；编写产品页面及详情页，详情页考虑在url中加入product_id的捕捉，编写detail视图。表单含图片，修改form标签的编码方式，服务器从request中获取数据，request.POST为含文本数据的字典，而request.files为含文件数据（图片）的字典，这与input标签的type相关联。

--- products/views.py
+++ products/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
+from .models import Product
 # Create your views here.
 
 
 def product_list(request):
-    return render(request, 'product_list.html')
+    products = Product.objects
+    return render(request, 'product_list.html', {'products':products})
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'detail.html', {"product":product})
+
+@login_required
+def publish(request):
+    if request.method == 'GET':
+        return render(request, 'publish.html')
+    elif request.method == 'POST':
+        title    = request.POST['标题']
+        intro    = request.POST['介绍']
+        link     = request.POST['链接']
+        try:
+            icon     = request.FILES['小图']
+            image    = request.FILES['大图']
+            product = Product()
+            product.title = title
+            product.intro = intro
+            product.link = link
+            product.icon = icon
+            product.image = image
+
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+
+            product.save()
+
+            return redirect('主页')
+        except Exception:
+            return render(request, 'publish.html', {'错误信息':'请上传图片'})
+
+       
